de85d76c46ead79db747615302b4c4ff6dd3ef12
RaymundOfina <Raymund.ofina@gmail.com>
2022-05-28 14:25:24 -0700

    Having issues connecting

--- server.js
+++ server.js
@@ -14,3 +14,12 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`localhost:${PORT}`));
