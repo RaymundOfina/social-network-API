e6bed0b88adee77cb1352ca4725d990f26217472
RaymundOfina <Raymund.ofina@gmail.com>
2022-06-02 15:47:46 -0700

    Fixing params

--- server.js
+++ server.js
@@ -1,80 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
 const routes = require('./routes');
 
-const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-const db = require('./models');
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/populatedb',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-mongoose.set('debug', true);
-
-// A user has been created already for our activity purposes
-db.User.create({ name: 'Ernest Hemingway' })
-  .then(dbUser => {
-    console.log(dbUser);
-  })
-  .catch(({ message }) => {
-    console.log(message);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
-
-// Retrieve all notes
-app.get('/notes', (req, res) => {
-  db.Note.find({})
-    .then(dbNote => {
-      res.json(dbNote);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-// Retrieve all users
-app.get('/user', (req, res) => {
-  db.User.find({})
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-// Create a new note and associate it with user
-app.post('/submit', ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) =>
-      db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true })
-    )
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get('/populate', (req, res) => {
-  // Write the query to `find()` all of the users from the User collection
-  // and `populate()` them with any associated notes.
-  // YOUR CODE HERE
-  //
-});
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
 });
