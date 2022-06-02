0a1bf6c6541e687f8feaf67936d130b04645c8fd
RaymundOfina <Raymund.ofina@gmail.com>
2022-06-02 15:35:09 -0700

    file changes

--- server.js
+++ server.js
@@ -15,11 +15,4 @@ db.once('open', () => {
   });
 });
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-mongoose.set('debug', true);
-
 app.listen(PORT, () => console.log(`localhost:${PORT}`));
