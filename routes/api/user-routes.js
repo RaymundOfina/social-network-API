1072075c255cb9136a7276c01f9e80c97c130318
RaymundOfina <Raymund.ofina@gmail.com>
2022-06-09 16:08:56 -0700

    fixing connection issues

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,21 +1,38 @@
 const router = require('express').Router();
+
 const {
-  getUsers,
-  getSingleUser,
-  createUser,
-  updateUser,
-  deleteUser,
-  addFriend,
-  removeFriend,
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
 } = require('../../controllers/user-controller');
 
-// /api/users
-router.route('/').get(getUsers).post(createUser);
+// Set up GET all and POST at /api/users
+router
+    .route('/')
+    .get()
+    .post();
 
-// /api/users/:userId
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
 
 // /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+// /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
 
 module.exports = router;
--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,7 +1,7 @@
 const addDateSuffix = date => {
   let dateStr = date.toString();
 
-  // get last char of date string
+  // obtains the last char
   const lastChar = dateStr.charAt(dateStr.length - 1);
 
   if (lastChar === '1' && dateStr !== '11') {
@@ -17,7 +17,7 @@ const addDateSuffix = date => {
   return dateStr;
 };
 
-// function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
+// timestamp, accepts timestamp and options
 module.exports = (
   timestamp,
   { monthLength = 'short', dateSuffix = true } = {}
@@ -70,7 +70,7 @@ module.exports = (
   const year = dateObj.getFullYear();
 
   let hour;
-  // check for 24-hr time
+  // 24-hr tine perameter check
   if (dateObj.getHours() > 12) {
     hour = Math.floor(dateObj.getHours() % 12);
   } else {
